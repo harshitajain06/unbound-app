fd6be5ce25ba984d3fd6d4f3c57db4dad8a36fc0
aishaani-agarwal <aishaani@gmail.com>
2024-05-07 13:31:40 +0530

    mobile compaitbility changes

--- components/Box.js
+++
@@ -1,9 +0,0 @@
-// components/Box.js
-import React from 'react';
-import styles from '../styles/Box.module.css';
-
-const Box = ({ children }) => {
-  return <div className={styles.Box}>{children}</div>;
-};
-
-export default Box;
